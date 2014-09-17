516e25a351ab45cd24f49ca79ad4c0bd1f4c5f53
Alexandru Albu <albu@google.com>
2014-09-17 09:35:03 -0700

    various changes

--- apkpackager.js
+++ apkpackager.js
@@ -15,14 +15,6 @@ module.exports.buildAPK =  function(zipPath, assetsPath, outputPath, templatePat
 }
 /*
 module.exports.makeapk = function(name) {
-    // need a native compatible absolute path that ends with /
-    /*
-      in the working directory I will have a
-      - template dirctory containing everything that is in the APK - the class.dex 
-      - class.dex
-      - the provided signing keys
-     */
-    // the only thing require to pass will be the workdir and the path to the keys.
     var workdir = 'file:///storage/sdcard0/Download/test-apk/'; //fs.root.toURL()+'Download/';
     var publicKeyURL = workdir+"pub.x509.pem";
     var privateKeyURL = workdir+"pk8p.pk8";
